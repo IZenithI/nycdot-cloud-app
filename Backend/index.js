291cb98fead65e7e209837d0c754e872741a4ac8
Brian Guo <brianguo810@gmail.com>
2021-10-15 18:14:56 -0400

    Implemented SignUp/SignIn Functionality

--- Backend/index.js
+++ Backend/index.js
@@ -2,31 +2,51 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const User = require('./Models/User');
+
+/////////////Middlewares/////////////////
+app.use(cors());
+app.use(express.json());
 
 /////////////CONNECT TO DB////////////////
 mongoose.connect(process.env.DB_CONNECTION, ()=>{
     console.log('Conencted to DB');
 })
 
-app.use(express.json());
-
-const User = require('./Models/User');
-
 /////////////////////////////////////////////
 app.get('/', function(req, res){
     res.send('Hello World!');
 });
 
-app.post('/signup', (req, res) => {
-    let email = req.body.email;
+app.post('/signup', async (req, res) => {
+    const user = await User.findOne({ 'email': req.body.email }).exec();
 
-    const user = new User({
-        email: email
-    })
-    
-    user.save();
+    if(user){
+        res.status(400).send("Already Signed Up");
+    }
+    else{
+        let email = req.body.email;
 
-    res.status(200).send(email + " Successfully Signed Up");
+        const user = new User({
+            email: email
+        })
+        
+        const savedUser = await user.save();
+
+        res.status(200).send(savedUser.email + " Successfully Signed Up");
+    }
+});
+
+app.post('/signin', async (req, res) => {
+    const user = await User.findOne({ 'email': req.body.email }).exec();
+
+    if(user){
+        res.status(200).send("Success");
+    }
+    else{
+        res.status(404).send("User is not found");
+    }
 });
 
 
