295fb051e70f497b9e6941611b14fa2899f7ec65
Brian Guo <brianguo810@gmail.com>
2021-10-15 17:43:50 -0400

    Connect to Database

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,32 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+/////////////CONNECT TO DB////////////////
+mongoose.connect(process.env.DB_CONNECTION, ()=>{
+    console.log('Conencted to DB');
+})
 
+app.use(express.json());
+
+const User = require('./Models/User');
+
+/////////////////////////////////////////////
 app.get('/', function(req, res){
     res.send('Hello World!');
+});
+
+app.post('/signup', (req, res) => {
+    let email = req.body.email;
+
+    const user = new User({
+        email: email
+    })
+    
+    user.save();
+
+    res.status(200).send(email + " Successfully Signed Up");
 });
 
 
